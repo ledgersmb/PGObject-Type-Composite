66c544027b4461e1baa56f70b9af21e2d5d35b95
Chris Travers <chris@efficito.com>
2014-08-31 00:36:18 -0700

    First test cases

--- lib/PGObject/Type/Composite.pm
+++ lib/PGObject/Type/Composite.pm
@@ -145,12 +145,16 @@ sub import {
         }
         return 1;
     };
+    my $_get_cols = sub {
+        return @cols;
+    };
 
     no strict 'refs';
     *{ "${caller}::initialize" } = $initialize;
     *{ "${caller}::register" }   = $register;
     *{ "${caller}::from_db" }    = $from_db;
     *{ "${caller}::to_db" }      = $to_db;
+    *{ "${caller}::_get_cols" }  = $_get_cols;
 }
 
 =head1 AUTHOR
