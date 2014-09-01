1c02f094c80f69a82eee0bbff567896262b395a2
Chris Travers <chris@efficito.com>
2014-08-31 17:31:35 -0700

    Packaging fixes for 0.02

--- lib/PGObject/Type/Composite.pm
+++ lib/PGObject/Type/Composite.pm
@@ -12,11 +12,11 @@ PGObject::Type::Composite - Composite Type handler for PGObject
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
